8897dcf0badc7bd8c4a94557afe9133d36b92a90
Eero Häihälä <eero.haihala@gmail.com>
2019-12-30 11:49:58 +0200

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate piston;
+
+use piston::window::*;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use graphics::*;
+use glutin_window::GlutinWindow as Window;
+use crate::piston::RenderEvent;
+
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new("Hello Piston!", [640, 480])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    let mut gl = GlGraphics::new(opengl);
+    let mut events = Events::new(EventSettings::new());
+    while let Some(event) = events.next(&mut window) {
+        if let Some(args) = event.render_args() {
+            gl.draw(args.viewport(), |context, graphics| {
+                clear([1.0; 4], graphics);
+                rectangle([1.0, 0.0, 0.0, 1.0], // red
+                          [0.0, 0.0, 100.0, 100.0],
+                          context.transform,
+                          graphics);
+            });
+        }
+    }
 }
